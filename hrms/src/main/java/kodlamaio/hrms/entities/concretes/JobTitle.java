9e2c9050dd4749e88b51d36becb581329c6ca41b
Cevat Sarıkaya <cvtsrky38@gmail.com>
2021-05-23 16:12:02 +0300

    Added Swagger

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
@@ -3,31 +3,27 @@ package kodlamaio.hrms.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name="job_titles")
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobTitle {
 		
 		@Id
-		@GeneratedValue
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		@Column(name="id")
 		private int id;
 		
 		@Column(name="name")
-		private String jobName;
+		private String jobName;		
 		
-		public JobTitle() {
-			
-		}
-
-		public JobTitle(int id, String jobName) {
-			super();
-			this.id = id;
-			this.jobName = jobName;
-		}
 }
